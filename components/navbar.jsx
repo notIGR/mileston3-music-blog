2d4bad256a37d6a7230a523ffea74550a2e0eb6f
Isaac Rodriguez <isaac.bh.rod@gmail.com>
2022-11-03 12:20:46 -0400

    added tailwind css and imporved navbar

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,29 +1,62 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 function Navbar() {
     return (
-        <ul>
-            <li>
-                <Link href='/'>
-                    Home
-                </Link></li>
-            <li>
-                <Link href='/about'>
-                    About
-                </Link>
-            </li>
-            <li>
-                <Link href='/profile'>
-                    Profile
-                </Link>
-            </li>
-            <li>
-                <Link href='/blog'>
-                    Blog
-                </Link>
-            </li>
-        </ul>
+        <>
+            <nav className='bg-black shadow-lg'>
+                <div className='container mx-auto'>
+                    <div className='sm:flex justify-around'>
+                        <Link href="/" className='text-white text-3xl font-bold p-3'>
+                            <Image
+                                priority
+                                src="/images/headphones.svg"
+                                height={50}
+                                width={50}
+                                alt="headphones"
+                            />
+                            MUSIC BLOG</Link>
+                        <ul className='text-gray-400 sm:self-center text-xl border-t sm:border-none'>
+                            <li className='sm:inline-block'>
+                                <Link href="/" className='p-3 hover:text-white'>Home</Link>
+                            </li>
+                            <li className='sm:inline-block'>
+                                <Link href="/profile" className='p-3 hover:text-white'>Profile</Link>
+                            </li>
+                            <li className='sm:inline-block'>
+                                <Link href="/about" className='p-3 hover:text-white'>About</Link>
+                            </li>
+                            <li className='sm:inline-block'>
+                                <Link href="/blog" className='p-3 hover:text-white'>Blog</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </>
     )
 }
 
 export default Navbar
+
+{/* <ul>
+<li>
+    <Link href='/'>
+        Home
+    </Link></li>
+<li>
+    <Link href='/about'>
+        About
+    </Link>
+</li>
+<li>
+    <Link href='/profile'>
+        Profile
+    </Link>
+</li>
+<li>
+    <Link href='/blog'>
+        Blog
+    </Link>
+</li>
+</ul> */}
--- pages/profile.jsx
+++ pages/profile.jsx
@@ -3,20 +3,20 @@ import Head from 'next/head';
 import utilStyles from '../styles/utils.module.css';
 
 function Profile() {
-    return (
-        <Layout home>
-        <Head>
-          <title>{siteTitle}</title>
-        </Head>
-        <section className={utilStyles.headingMd}>
-          <p>"I love music", now thats in understament. Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt debitis nulla excepturi, blanditiis saepe sequi perspiciatis, ipsa minus delectus, ab enim id? Fuga consequatur, alias fugiat dolores ea quisquam quaerat? Nisi in repudiandae odit perspiciatis accusamus, iste sequi nemo esse et voluptas earum culpa fugiat maiores velit tenetur vel? Maiores.</p>
-          <p>
-            This is currnetly my go to song...
-            <a href="https://open.spotify.com/track/3dr3IVp8wzEHbcOJ4qdqxd?si=b67e5f1f8e6f4444" target="blank">PUCKER UP</a>
-          </p>
-        </section>
-      </Layout>
-    )
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>"I love music", now thats in understament. Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt debitis nulla excepturi, blanditiis saepe sequi perspiciatis, ipsa minus delectus, ab enim id? Fuga consequatur, alias fugiat dolores ea quisquam quaerat? Nisi in repudiandae odit perspiciatis accusamus, iste sequi nemo esse et voluptas earum culpa fugiat maiores velit tenetur vel? Maiores.</p>
+        <p>
+          This is currnetly my go to song...
+          <a href="https://open.spotify.com/track/3dr3IVp8wzEHbcOJ4qdqxd?si=b67e5f1f8e6f4444" target="blank">PUCKER UP</a>
+        </p>
+      </section>
+    </Layout>
+  )
 }
 
 export default Profile
