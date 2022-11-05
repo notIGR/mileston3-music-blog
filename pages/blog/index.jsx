26eff2accf8ca9b37a376a2bae4a516d5dee78cb
Isaac Rodriguez <isaac.bh.rod@gmail.com>
2022-11-05 14:33:25 -0400

    added some styling and links to blog articles

--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -1,7 +1,15 @@
+import utilStyles from '../../styles/utils.module.css';
+import Link from 'next/link'
 
 function Blog() {
     return (
-        <h1>Blog Page</h1>
+        <section className={utilStyles.headingMd}>
+            <h1 className='text-center'>Here is a list of what I have written so far...</h1>
+            <ul className='text-gray-400 sm:self-center text-xl border-t sm:border-none p-4 space-x-4 text-center'>
+                <li className='sm:inline-block'><Link href='/blog/first'>My First Music Festival</Link></li>
+                <li className='sm:inline-block'><Link href='/blog/second'>How to get others involved</Link></li>
+            </ul>
+        </section>
     )
 }
 
