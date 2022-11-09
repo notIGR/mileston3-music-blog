4d6be160b4f0b27efe5847d2ba93803eeb688348
Isaac Rodriguez <isaac.bh.rod@gmail.com>
2022-11-09 17:18:42 -0500

    changed profile picture so it can load even if not locally

--- components/layout.jsx
+++ components/layout.jsx
@@ -30,7 +30,7 @@ export default function Layout({ children, home }) {
           <>
             <Image
               priority
-              src="/images/profile.jpg"
+              src="/images/music-spell.svg"
               className={utilStyles.borderCircle}
               height={144}
               width={144}
