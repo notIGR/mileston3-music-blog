be827abdef93c6e7bcb98f53ff3141d1147eb56d
Isaac Rodriguez <isaac.bh.rod@gmail.com>
2022-11-09 19:15:34 -0500

    added pointer

--- components/newsletter.jsx
+++ components/newsletter.jsx
@@ -5,9 +5,9 @@ export default function Newsletter() {
             <div className="container mx-auto md:px-20 py-16 text-center">
                 <h1 className="font-bold text 3xl">Sub to our newsletter!</h1>
                 <div className="py-4">
-                    <input type="text" className="shadow border rounded w-9/12 py-3 px=3 text-gray-800 focus:outline-none focus:shadow-outline" placeholder="Enter Your Email" />
+                    <input type="email" className="shadow border rounded w-9/12 py-3 px=3 text-gray-800 focus:outline-none focus:shadow-outline cursor-pointer" placeholder="Enter Your Email" />
                 </div>
-                <button className="bg-gray-400 px-20 py-3 rounded-full text-gray-5 text-xl"> Subscribe</button>
+                <button type="submit"className="bg-gray-400 px-20 py-3 rounded-full text-gray-5 text-xl"> Subscribe</button>
             </div>
         </section>
     )
